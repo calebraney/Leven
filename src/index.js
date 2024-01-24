10ed07e9b84724a3c83f2ea3dd78d389653aceb1
Caleb Raney <110679107+calebraney@users.noreply.github.com>
2024-01-24 17:41:05 -0500

    added cursor

--- src/index.js
+++ src/index.js
@@ -84,7 +84,7 @@ window.Webflow.push(() => {
     });
   };
 
-  function solutionsScroll() {
+  const solutionsScroll = function () {
     const tabLinks = gsap.utils.toArray('[cr-split-link]');
     const processItems = gsap.utils.toArray('[cr-split-content]');
     const processImages = gsap.utils.toArray('[cr-split-image]');
@@ -154,7 +154,83 @@ window.Webflow.push(() => {
         },
       });
     });
-  }
+  };
+
+  const cursor = function () {
+    // select the items
+    const cursorWrap = document.querySelector('[cursor="component"]');
+    const cursorInner = document.querySelector('[cursor="inner"]');
+    const cursorDot = document.querySelector('[cursor="dot"]');
+    const HOVER_CLASS = 'is-hover';
+
+    // return if items are null
+    if (!cursorWrap || !cursorInner) return;
+
+    const cursorHover = function () {
+      // get all links without a no-hover attribute and any other elements with a hover attribute into an array
+      const hoverElements = gsap.utils.toArray(
+        '[cursor="hover"], :is(a, button):not([cursor="no-hover"])'
+      );
+      hoverElements.forEach((item) => {
+        if (!item || !cursorDot) return;
+        item.addEventListener('mouseover', function (e) {
+          cursorDot.classList.add(HOVER_CLASS);
+        });
+        item.addEventListener('mouseleave', function (e) {
+          cursorDot.classList.remove(HOVER_CLASS);
+        });
+      });
+    };
+    cursorHover();
+    const cursorClick = function () {
+      if (!cursorDot) return;
+      document.addEventListener('click', function (e) {
+        let tl = gsap.timeline({});
+        tl.fromTo(cursorDot, { rotateZ: -45 }, { rotateZ: 45, ease: 'power1.out', duration: 0.3 });
+      });
+    };
+    cursorClick();
+
+    //handle cursor movement
+    const cursorMove = function () {
+      // object that stores the value of the progress so it can be animated
+      let progressObject = { x: 0, y: 0 };
+
+      // Create X timeline
+      let cursorXTimeline = gsap.timeline({ paused: true, defaults: { ease: 'none' } });
+      cursorXTimeline.fromTo(cursorInner, { x: '-50vw' }, { x: '50vw' });
+      // Create Y Timeline
+      let cursorYTimeline = gsap.timeline({ paused: true, defaults: { ease: 'none' } });
+      cursorYTimeline.fromTo(cursorInner, { y: '-50vh' }, { y: '50vh' });
+
+      // Function to update timeline progress based on an inputted value
+      function setTimelineProgress(xValue, yValue) {
+        // animate the timeline progress value and keep the timeline in sync onUpdate
+        gsap.to(progressObject, {
+          x: xValue,
+          y: yValue,
+          ease: 'none',
+          duration: 0,
+          onUpdate: () => {
+            cursorXTimeline.progress(progressObject.x);
+            cursorYTimeline.progress(progressObject.y);
+          },
+        });
+        // an alternate option if you want the timeline progress to jump immediately without easing (will be choppier)
+        // tl.progress(progressValue)
+      }
+
+      // Mouse events
+      document.addEventListener('mousemove', function (e) {
+        // getting the horizontal and vertical positions of the mouse and dividing it by the total screen width
+        let mousePercentX = e.clientX / window.innerWidth;
+        let mousePercentY = e.clientY / window.innerHeight;
+        // call function to animate the timeline progress
+        setTimelineProgress(mousePercentX, mousePercentY);
+      });
+    };
+    cursorMove();
+  };
 
   // const gsapInit = function () {
   mm.add(
@@ -166,14 +242,17 @@ window.Webflow.push(() => {
       reduceMotion: '(prefers-reduced-motion: reduce)',
     },
     (context) => {
-      let { isMobile, isTablet, isDesktop, reduceMotion } = context.conditions;
       clearFiltersOnClick();
+      let { isMobile, isTablet, isDesktop, reduceMotion } = context.conditions;
       updateProjectColors();
       moveNavbarBg();
       matchSectionNavColor(isMobile);
       solutionsScroll();
       //Run if reduce motion is off
       if (!reduceMotion) {
+      }
+      if (isDesktop || isTablet) {
+        cursor();
       }
     }
   );
